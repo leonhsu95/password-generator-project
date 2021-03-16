5dbfb3518fd396c5599583f8ec8e6052bc19d863
Leon Hsu <leonhsu95@gmail.com>
2021-03-16 17:46:50 +1100

    Checking user password input and if user cancels function.

--- scripts/script.js
+++ scripts/script.js
@@ -7,15 +7,40 @@ var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M"
 var number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var specialChar = [" ", "!", "\"", "#", "$", "%", "&", "\'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`", "{", "|", "}", "~"];
 
+function writePassword(){
+  var generatePassword = prompt("How many charcters between 8-128 would you like your password to be?");
+
+  if (!generatePassword) {
+    var cancel = confirm("Cancel Password Generation?");
+
+    if (cancel) {
+      alert("Password Generation cancelled.");
+      return;
+    }
+    else {
+      var generatePassword = prompt("How many charcters between 8-128 would you like your password to be?");
+    }
+  }
+  else if (generatePassword < 8 || generatePassword > 128 ) {
+    alert("Password length must be between 8-128 characters. Try again");
+    var generatePassword = prompt("How many charcters between 8-128 would you like your password to be?");
+    
+  }
+  else {
+    alert("Your password will have "+generatePassword+" characters.");
+    
+  }
+  
+};
 
 // Write password to the #password input
-function writePassword() {
+/*function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
-}
+}*/
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
