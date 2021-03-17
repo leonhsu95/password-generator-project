95a4d1408af6232bbf1b24a7f282440107a35c41
Leon Hsu <leonhsu95@gmail.com>
2021-03-17 18:44:40 +1100

    All character type choices concatenated when confirmed and randomised.

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,14 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var passwordText = document.querySelector("#password");
 
-//Character Variables stored in Arrays
+// Character Variables stored in Arrays
 var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-var specialChar = [" ", "!", "\"", "#", "$", "%", "&", "\'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`", "{", "|", "}", "~"];
+var specialChar = ["!", "\"", "#", "$", "%", "&", "\'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`", "{", "|", "}", "~"];
 
-function writePassword(){
+function writePassword() {
   var generatePassword = prompt("How many characters between 8-128 would you like your password to be?");
 
   if (!generatePassword) {
@@ -32,7 +33,7 @@ function writePassword(){
   
   alert("Your password will have "+generatePassword+" characters.");
 
-  //Confirm if user wants certain character type variables
+  // Confirm if user wants certain character type variables
   var confirmLowerCase = confirm("Click OK to confirm if you would like to include lower case characters.");
   var confirmUpperCase = confirm("Click OK to confirm if you would like to include upper case characters.");
   var confirmNumber = confirm("Click OK to confirm if you would like to include numbers.");    
@@ -46,20 +47,37 @@ function writePassword(){
     var confirmSpecialChar = confirm("Click OK to confirm if you would like to include special characters.");
   } 
   
+  // Password parameters
+  var passwordChar = [];
+
+  // Password to keep adding character type onto existing    
+  if (confirmLowerCase){
+    passwordChar = passwordChar.concat(lowerCase);
+  }
+  if (confirmUpperCase){
+    passwordChar = passwordChar.concat(upperCase);
+  }
+  if (confirmNumber){
+    passwordChar = passwordChar.concat(number);
+  }
+  if (confirmSpecialChar){
+    passwordChar = passwordChar.concat(specialChar);
+  }
+
+  // Password must be a string of characters.
+   var newPassword = "";
   
+  // Array loop for total length of Password desired
+  for (let i = 0; i < generatePassword; i++) {
+
+  // Each character type is chosen from variable array and chosen randomly.
+  // Since Math.random() chooses decimal numbers between 0 - 1, we want to round down into an integer between 8 - 128
+   newPassword = newPassword + passwordChar[Math.floor(Math.random() * passwordChar.length)];
+   passwordText.value = newPassword;
+
+  }
   
 };
-
-
-
-// Write password to the #password input
-/*function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}*/
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
